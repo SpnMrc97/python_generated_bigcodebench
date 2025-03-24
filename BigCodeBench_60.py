2f65156ccaff8672f5d4d92592a6ba95102a1a19
SpnMrc97 <marcospina.u@gmail.com>
2025-03-24 14:32:02 +0100

    Add files via upload

--- BigCodeBench_60.py
+++ BigCodeBench_60.py
@@ -12,8 +12,3 @@ def task_func(result, csv_file_path="test.csv", json_file_path="test.json"):
     # Save the list of dictionaries to a JSON file
     with open(json_file_path, 'w') as json_file:
         json.dump(result, json_file, indent=4)
-
-# Example usage
-if __name__ == "__main__":
-    result = [{"hi": 7, "bye": 4, "from_user": 0}, {1: 2, 3: 4, 5: 6}]
-    task_func(result, 'test.csv', 'test.json')
